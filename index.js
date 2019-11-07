2f30fe4c9961551a6f96729b0463fb61aee99206
Manu Álvarez <malvarez@Manus-MacBook.local>
2019-11-07 10:28:12 +0100

    created class block

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import PKG from './package.json';
+import Block from './src/blockchain/block';
 
-const {name, version} = PKG;
-console.log(`${name} - ${version}`);
+const block = new Block(Date.now(), '1i23849281uy4892y19834y', '1823742384789357', {});
+console.log(block.toString());
